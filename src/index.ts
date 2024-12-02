0a56e10015a47c7100ebf7e16b493ac829eae690
bipproduction <bip.production.js@gmail.com>
2024-12-02 10:26:41 +0800

    tambah minimist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 import { sortServer } from "./lib/sort-server";
+import minimist from "minimist";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const argv = minimist(process.argv.slice(2));
+const port = argv.port || argv.p || 3000;
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
 app.get("/sort-server", sortServer);
 
 console.log(
